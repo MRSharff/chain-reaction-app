8d3fda5b09257c524224c61118506550eef49a07
Mat Sharff <mrsharff@uw.edu>
2016-04-28 22:35:30 -0700

    Added game settings menu for the screen after singleplayer. Clicking Single Player afte rlogging in takes you to GameSettingsActivity

---
+++ app/src/main/java/a450sp16team2/tacoma/uw/edu/chainreaction/GameSettingsActivity.java
@@ -0,0 +1,31 @@
+package a450sp16team2.tacoma.uw.edu.chainreaction;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.AppCompatSpinner;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+
+public class GameSettingsActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = GameSettingsActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_settings);
+
+        AppCompatSpinner difficultySpinner = (AppCompatSpinner) findViewById(R.id.difficulty_spinner);
+
+        ArrayAdapter<CharSequence> diffSpinnerAdapter = ArrayAdapter.createFromResource(this,
+                R.array.classic_difficulty,
+                android.R.layout.simple_spinner_item);
+
+        try {
+
+            difficultySpinner.setAdapter(diffSpinnerAdapter);
+        } catch (NullPointerException e) {
+            Log.e(LOG_TAG, "Difficulty Spinner was null");
+        }
+    }
+}
--- app/src/main/java/a450sp16team2/tacoma/uw/edu/chainreaction/HomeActivity.java
+++ app/src/main/java/a450sp16team2/tacoma/uw/edu/chainreaction/HomeActivity.java
@@ -1,9 +1,11 @@
 package a450sp16team2.tacoma.uw.edu.chainreaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
@@ -13,6 +15,17 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        AppCompatButton singlePlayerButton = (AppCompatButton) findViewById(R.id.btn_singleplayer);
+
+        singlePlayerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSinglePlayer();
+
+            }
+        });
+
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 //
@@ -24,6 +37,11 @@ public class HomeActivity extends AppCompatActivity {
 //                        .setAction("Action", null).show();
 //            }
 //        });
+    }
+
+    private void startSinglePlayer() {
+        Intent intent = new Intent(this, GameSettingsActivity.class);
+        startActivity(intent);
     }
 
 }
