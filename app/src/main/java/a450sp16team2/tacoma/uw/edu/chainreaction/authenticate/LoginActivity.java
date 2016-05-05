1ba5bc593712424e9df7abff1bf5befe2504bff0
Mat Sharff <mrsharff@uw.edu>
2016-05-05 10:51:30 -0700

    Web login connected to cssgate database.

--- app/src/main/java/a450sp16team2/tacoma/uw/edu/chainreaction/authenticate/LoginActivity.java
+++ app/src/main/java/a450sp16team2/tacoma/uw/edu/chainreaction/authenticate/LoginActivity.java
@@ -16,13 +16,26 @@ import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Base64;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,6 +43,9 @@ import a450sp16team2.tacoma.uw.edu.chainreaction.HomeActivity;
 import a450sp16team2.tacoma.uw.edu.chainreaction.R;
 
 public class LoginActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final String OFFLINE_USERNAME = "OFFLINEUSER";
+    private static final String OFFLINE_PASSWORD = "OFFLINEPASS";
 
     private static final String LOG_TAG = LoginActivity.class.getSimpleName();
 
@@ -65,7 +81,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderManager.Lo
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
                 if (id == R.id.login || id == EditorInfo.IME_NULL) {
-                    attemptLogin();
+                    attemptLogin(false);
                     return true;
                 }
                 return false;
@@ -76,7 +92,15 @@ public class LoginActivity extends AppCompatActivity implements LoaderManager.Lo
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                attemptLogin();
+                attemptLogin(false);
+            }
+        });
+
+        Button mPlayOfflineButton = (Button) findViewById(R.id.btn_play_offline);
+        mPlayOfflineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin(true);
             }
         });
 
@@ -91,50 +115,57 @@ public class LoginActivity extends AppCompatActivity implements LoaderManager.Lo
      * If there are form errors (invalid username, missing fields, etc.), the
      * errors are presented and no actual login attempt is mode.
      */
-    private void attemptLogin() {
+    private void attemptLogin(boolean isOffline) {
         if (mAuthTask != null) {
             return;
         }
 
-        // Reset errors.
-        mUsernameView.setError(null);
-        mPasswordView.setError(null);
-
-        // Store values at the time of the login attempt.
-        String username = mUsernameView.getText().toString();
-        String password = mPasswordView.getText().toString();
-
-        boolean cancel = false;
-        View focusView = null;
-
-        // check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-            mPasswordView.setError(getString(R.string.error_invalid_username));
-            focusView = mPasswordView;
-            cancel = true;
-        }
-
-        // Check for a valid username.
-        if (TextUtils.isEmpty(username)) {
-            mUsernameView.setError(getString(R.string.error_field_required));
-            focusView = mUsernameView;
-            cancel  = true;
-        } else if (!isUsernameValid(username)) {
-            mUsernameView.setError(getString(R.string.error_invalid_username));
-            focusView = mUsernameView;
-            cancel = true;
-        }
-
-        if  (cancel) {
-            // There was an error; don't attempt login and focus the first
-            // form field with an error.
-            focusView.requestFocus();
-        } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
+        if (isOffline) {
             showProgress(true);
-            mAuthTask = new UserLoginTask(username, password, this);
+            mAuthTask = new UserLoginTask(OFFLINE_USERNAME, OFFLINE_PASSWORD, this);
             mAuthTask.execute((Void) null);
+        } else {
+
+            // Reset errors.
+            mUsernameView.setError(null);
+            mPasswordView.setError(null);
+
+            // Store values at the time of the login attempt.
+            String username = mUsernameView.getText().toString();
+            String password = mPasswordView.getText().toString();
+
+            boolean cancel = false;
+            View focusView = null;
+
+            // check for a valid password, if the user entered one.
+            if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+                mPasswordView.setError(getString(R.string.error_invalid_username));
+                focusView = mPasswordView;
+                cancel = true;
+            }
+
+            // Check for a valid username.
+            if (TextUtils.isEmpty(username)) {
+                mUsernameView.setError(getString(R.string.error_field_required));
+                focusView = mUsernameView;
+                cancel = true;
+            } else if (!isUsernameValid(username)) {
+                mUsernameView.setError(getString(R.string.error_invalid_username));
+                focusView = mUsernameView;
+                cancel = true;
+            }
+
+            if (cancel) {
+                // There was an error; don't attempt login and focus the first
+                // form field with an error.
+                focusView.requestFocus();
+            } else {
+                // Show a progress spinner, and kick off a background task to
+                // perform the user login attempt.
+                showProgress(true);
+                mAuthTask = new UserLoginTask(username, password, this);
+                mAuthTask.execute((Void) null);
+            }
         }
 
     }
@@ -234,6 +265,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderManager.Lo
      */
     public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
 
+        private final static String AUTHENTICATE_URL =
+                "http://cssgate.insttech.washington.edu/~mrsharff/Android/crlogin.php?";
+
         private final String mUsername;
         private final String mPassword;
 
@@ -248,23 +282,30 @@ public class LoginActivity extends AppCompatActivity implements LoaderManager.Lo
         @Override
         protected Boolean doInBackground(Void... params) {
             // TODO: attempt authentication against a network service.
+            String shaPassword = getSHA256(mPassword);
+            Log.i(LOG_TAG, "SHA: " + shaPassword);
+            String response = "";
+            HttpURLConnection urlConnection = null;
+            String url = buildLoginURL(shaPassword);
 
             try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
+                URL urlObject = new URL(url);
+                urlConnection = (HttpURLConnection) urlObject.openConnection();
 
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mUsername)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
+                InputStream content = urlConnection.getInputStream();
+                BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                String s = buffer.readLine();
+                Log.i(LOG_TAG, "S: " + s);
+            } catch (Exception e) {
+                response = "Unable to login, reason: " + e.getMessage();
+                Log.e(LOG_TAG, response);
+                return false;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
                 }
             }
 
-            // TODO: register the new account here. (Or in RegisterActivity.java for ours)
             return true;
         }
 
@@ -287,6 +328,59 @@ public class LoginActivity extends AppCompatActivity implements LoaderManager.Lo
         protected void onCancelled() {
             mAuthTask = null;
             showProgress(false);
+        }
+
+        private String getSHA256(String password) {
+            MessageDigest mdSHA256 = null;
+            String shaHash = null;
+            try {
+                mdSHA256 = MessageDigest.getInstance("SHA-256");
+            } catch (NoSuchAlgorithmException e1) {
+                Log.e(LOG_TAG, "Error initializing SHA256 message digest");
+            }
+            try {
+                mdSHA256.update(password.getBytes("ASCII"));
+            } catch (UnsupportedEncodingException e2) {
+                e2.printStackTrace();
+            }
+
+            byte[] data = mdSHA256.digest();
+            try {
+                shaHash = convertToHex(data);
+            } catch (IOException e3) {
+                e3.printStackTrace();
+            }
+
+            return shaHash;
+        }
+
+        private String buildLoginURL(String password) {
+
+            StringBuilder sb = new StringBuilder(AUTHENTICATE_URL);
+
+            try {
+                sb.append("email=");
+                sb.append(mUsername);
+
+                sb.append("&pwd=");
+                sb.append(password);
+
+                Log.i("LoginURL", sb.toString());
+
+            }
+            catch(Exception e) {
+                Log.e(LOG_TAG, "Something wrong with the url" + e.getMessage());
+            }
+            return sb.toString();
+        }
+
+        private String convertToHex(byte[] theData) throws java.io.IOException {
+            StringBuffer stringBuffer = new StringBuffer();
+            String hex;
+            hex = Base64.encodeToString(theData, 0, theData.length, 0);
+
+            stringBuffer.append(hex);
+            return stringBuffer.toString();
         }
     }
 }
